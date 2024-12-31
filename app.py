2496d8598cd09c4b6e28480a39b49f660369bc74
Laurent Vong <laurent.vong@ensae.fr>
2024-12-31 01:04:21 +0100

    vercel

--- app.py
+++ app.py
@@ -110,4 +110,5 @@ def reset_ingredients():
     return jsonify(selected_ingredients)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
