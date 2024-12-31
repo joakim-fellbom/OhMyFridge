42ba4694bb16712c0fdc534a788123563abe4374
Laurent Vong <laurent.vong@ensae.fr>
2024-12-31 03:04:20 +0100

    backslash_final

--- app.py
+++ app.py
@@ -51,9 +51,9 @@ def get_recipes():
     try:
         ingredients_lower = [ing.lower() for ing in selected_ingredients]
         #ingredients_str = str(ingredients_lower).replace("'", "\"")
-        ingredients_formatted = [f'"{ing.lower()}"' for ing in selected_ingredients]
+        ingredients_formatted = [f'\"{ing.lower()}\"' for ing in selected_ingredients]
         ingredients_joined = ", ".join(ingredients_formatted)
-        ingredients_str = f'[\"{ingredients_joined}\"]'
+        ingredients_str = f"[{ingredients_joined}]"
         
         if exact_match:
             # For exact match using $iContains
